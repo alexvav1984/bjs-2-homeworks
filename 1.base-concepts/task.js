4f9e7d32af49537e4a4200d94564d3def2a01dc1
Vasilev Aleksei <38896658+alexvav1984@users.noreply.github.com>
2023-08-17 19:30:03 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,21 +7,21 @@ let d = Math.pow(b, 2)-(4*a*c);
 
 if (d < 0) {
   arr = [];
-};
+}
 
 if (d === 0) {
   let xone = -b/(2*a);
   arr = [xone];
-};
+}
 
 if (d > 0) {
   let xone = (-b + Math.sqrt(d) )/(2*a);
   let xtwo = (-b - Math.sqrt(d) )/(2*a);
   arr = [xone, xtwo];
-};
+}
 
   return arr;
-};
+}
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 let percentMonth =  percent / 100 / 12;
@@ -30,4 +30,4 @@ let pay = body * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMo
 let summCredit = pay * countMonths;
 
 return +summCredit.toFixed(2);
-};
+}
