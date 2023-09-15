5a68fe74818643d9d7337e142647bb248666847f
Vasilev Aleksei <38896658+alexvav1984@users.noreply.github.com>
2023-09-16 00:07:52 +0300

    Update task1.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,17 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+    let min = arr[0], max = arr[0], sum = 0;
+    for (i = 0; i < arr.length; i++){
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        sum += arr[i];
+    }
+    let avg = sum / arr.length;
+    +avg.toFixed(2);
+    return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
